7692af811236180a4e06c188242d6f713d6c9664
alzflost <alzflost@naver.com>
2022-11-27 20:04:32 +0900

    map edit mode 90% implement

--- Assets/Script/SnowmanGenerate.cs
+++ Assets/Script/SnowmanGenerate.cs
@@ -4,42 +4,64 @@ using UnityEngine;
 
 public class SnowmanGenerate : MonoBehaviour
 {
-    GameObject snowman_prefab;
+
+    public string str;
+    // GameObject snowman_prefab;
+    GameObject prefab_obj;
     Vector3 ScreenPos;
     Vector3 RealPos;
+
     void CalculatePos()
     {
-        snowman_prefab = Resources.Load<GameObject>("snowman");
         ScreenPos = Input.mousePosition;
-        // Debug.Log("SC : " + ScreenPos.x + " " + ScreenPos.y + " " + ScreenPos.z);
-        // RealPos = Camera.main.ScreenToWorldPoint(new Vector3(ScreenPos.x, ScreenPos.y,
-        //    Camera.main.transform.position.y - Terrain.activeTerrain.terrainData.GetHeight((int)ScreenPos.x, (int)ScreenPos.y)));
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitData;
         Physics.Raycast(ray, out hitData);
         RealPos = hitData.point;
         Debug.Log(RealPos.x + " " + RealPos.y + " " + RealPos.z);
-        //RealPos = Camera.main.ScreenToWorldPoint(new Vector3(ScreenPos.x, ScreenPos.y, 
-        //    Camera.main.transform.position.y - Terrain.activeTerrain.terrainData.GetHeight((int)ScreenPos.x, (int)ScreenPos.y)));
-
-
-        //Debug.Log("RE : " + RealPos.x + " " + RealPos.y + " " + RealPos.z);
-        // RealPos.y = Camera.main.transform.position.y - Terrain.activeTerrain.terrainData.GetHeight((int)RealPos.x, (int)RealPos.y);
-        //Debug.Log("RE2 : " + RealPos.x + " " + RealPos.y + " " + RealPos.z);
-        // Debug.Log(Terrain.activeTerrain.SampleHeight(Input.mousePosition));
-        // TargetPos = new Vector3(RealPos.x, 100f, RealPos.z);
     }
 
     void MoveToTarget()
     {
-        // Debug.Log("Pos : " + RealPos.x + " " + RealPos.y + " "+ RealPos.z);
-        GameObject bullet = Instantiate<GameObject>(snowman_prefab, new Vector3(RealPos.x, RealPos.y, RealPos.z), Quaternion.identity);
+        GameObject bullet = Instantiate<GameObject>(prefab_obj, new Vector3(RealPos.x, RealPos.y, RealPos.z), Quaternion.identity);
+    }
+
+    void NewObject()
+    {
+        prefab_obj = Resources.Load<GameObject>(str);
+        CalculatePos();
+        MoveToTarget();
+    }
+
+    public void ChangeToRocks()
+    {
+        str = "Rock";
+    }
+
+    public void ChangeToHouse()
+    {
+        str = "House";
+    }
+
+    public void ChangeToSnowman()
+    {
+        str = "Snowman";
+    }
+
+    public void ChangeToWell()
+    {
+        str = "Well";
+    }
+
+    public void ChangeToSwing()
+    {
+        str = "Swing";
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -47,8 +69,7 @@ public class SnowmanGenerate : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            CalculatePos();
-            MoveToTarget();
+            NewObject();
         }
     }
 }
